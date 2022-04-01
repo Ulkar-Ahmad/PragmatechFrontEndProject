f5e19e8dac7af1ef72233cde2459ba738efde895
Ulkar-Ahmad <ulkerahmedova02@gmail.com>
2022-04-01 18:48:48 +0400

    task-2 js code folder add

---
+++  JsDataStructures/js/task-2.js
@@ -0,0 +1,10 @@
+
+let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
+let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
+let ages = [23, 34, 40, 58, 60];
+// let students = [names, surnames, ages]
+
+
+for(let i=0; i<names.length; i++) {
+    console.log(`Ad:${names[i]}, Soyad: ${surnames[i]}, Yas: ${ages[i]}`);
+}
